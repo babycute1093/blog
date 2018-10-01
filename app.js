21a436b50e394da9c763bf104d2399552bc6ab3a
Cuong Tran <trandaohuycuong@gmail.com>
2018-10-02 02:55:17 +0700

    Init blog

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var config = require("config");
+var bodyParser = require("body-parser");
+
+var app = express();
+var controllers = require(__dirname + "/apps/controllers");
+var host = config.get("server.host");
+var port = config.get("server.port")
+
+app.use(controllers);
+app.use(bodyParser.json());
+
+app.set("views", __dirname + "/apps/views");
+app.set("view engine", "ejs");
+app.use("/static", express.static(__dirname + "/public"));
+
+app.listen(port, host, function(){
+    console.log("Server in running on port ", port)
+});
